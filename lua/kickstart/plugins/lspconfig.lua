bbc06c3c13f0de12cf293e6b8f2dacceab2efdf7
naftali <48069590+naftali100@users.noreply.github.com>
2025-05-14 01:43:10 +0300

    add python tooling

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -220,6 +220,8 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        basedpyright = {},
+        ruff = {},
 
         lua_ls = {
           -- cmd = { ... },
