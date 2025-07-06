dafbf91e3191f1f3e9314e5a7ff7246bc2317826
naftali <48069590+naftali100@users.noreply.github.com>
2025-07-07 01:04:05 +0300

    clean

--- lua/custom/plugins/misc.lua
+++ lua/custom/plugins/misc.lua
@@ -1,20 +1,21 @@
-return {
-  -- from https://github.com/mathletedev/nix-home/blob/main/src/nvim/lua/plugins/lsp.lua
-  {
-    'ray-x/lsp_signature.nvim',
-    event = 'InsertEnter',
-    opts = {
-      -- cfg options
-    },
-  },
-  {
-    'nvimdev/lspsaga.nvim',
-    config = function()
-      require('lspsaga').setup {}
-    end,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter', -- optional
-      'nvim-tree/nvim-web-devicons', -- optional
-    },
-  },
-}
+return {}
+-- return {
+--   -- from https://github.com/mathletedev/nix-home/blob/main/src/nvim/lua/plugins/lsp.lua
+--   {
+--     'ray-x/lsp_signature.nvim',
+--     event = 'InsertEnter',
+--     opts = {
+--       -- cfg options
+--     },
+--   },
+--   {
+--     'nvimdev/lspsaga.nvim',
+--     config = function()
+--       require('lspsaga').setup {}
+--     end,
+--     dependencies = {
+--       'nvim-treesitter/nvim-treesitter', -- optional
+--       'nvim-tree/nvim-web-devicons', -- optional
+--     },
+--   },
+-- }
